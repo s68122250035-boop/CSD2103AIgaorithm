e628fdc11527c30a4e9dfbb212a1cf831642eb98
นางสาว สุภัศวรรณ ท้วมมา 681222250035 <s68122250035@ssru.ac.th>
2026-02-26 10:54:26 +0700

    Add files via upload

---
+++ 035/Work/TestDemoException.java
@@ -0,0 +1,19 @@
+class MyException extends RuntimeException {
+    public MyException(String message) {
+        super(message); // ส่ง message ไป JVM
+    }
+}
+
+public class TestDemoException {
+   
+    static void checkAge(int age) {
+        if (age < 18) {
+            throw new MyException("Age must be at least 18");
+        }
+        System.out.println("Access granted");
+    }
+
+    public static void main(String[] args) {
+        checkAge(15);
+    }
+}
