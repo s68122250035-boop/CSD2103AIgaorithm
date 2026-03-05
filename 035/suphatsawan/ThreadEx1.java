3554d33d65480d620597d566734c534b33f937a9
นางสาว สุภัศวรรณ ท้วมมา 681222250035 <s68122250035@ssru.ac.th>
2026-03-05 09:04:06 +0700

    Add files via upload

---
+++ 035/suphatsawan/ThreadEx1.java
@@ -0,0 +1,8 @@
+public class ThreadEx1{
+ public static void main(String[] args) 
+ {
+ System.out.println("Hello world");
+ System.out.println("Thread name: " + Thread.currentThread().getName());     
+    }
+    
+}
